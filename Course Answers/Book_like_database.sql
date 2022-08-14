739d34cd0929ea503e89dd9492a9a0c6520d0224
Harsh-Kumar-Sinha <harshsinha1999@gmail.com>
2022-08-14 23:35:10 +0530

    Inserting data in a Table

--- Course Answers/Book_like_database.sql
+++ Course Answers/Book_like_database.sql
@@ -1,5 +1,6 @@
 create table books(id integer primary key,name text,rating integer);
 
 insert into books values(1,"Atomic Habits",5);
-insert into books values(2,"ChankyaNeeti",5);
+insert into books values(2,"Chankya Neeti",5);
 insert into books values(3,"Alchemist",4);
+insert into books values(4,"Think Like a Monk",5);
